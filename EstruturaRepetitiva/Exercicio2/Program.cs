180b4de74e97b99c9766c3e383359a49d794853d
Felipe Speranza <felipesperanza@hotmail.com>
2024-12-03 22:11:19 -0300

    Estrutura Repetitiva

---
+++ EstruturaRepetitiva/Exercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+double media = 0;
+int contador = 0;
+
+int idade = int.Parse(Console.ReadLine());
+
+while (idade > 0)
+{
+    media += idade;
+    contador++;
+    idade = int.Parse(Console.ReadLine());
+}
+
+if (contador == 0)
+{
+    Console.WriteLine("Impossível Calcular");
+}
+else
+{
+    media = media / contador;
+
+    Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+}
+
---
+++ EstruturaRepetitiva/URI1115/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] vet = Console.ReadLine().Split(' ');
+int X = int.Parse(vet[0]);
+int Y = int.Parse(vet[1]);
+
+while (X != 0 && Y != 0)
+{
+    if (X > 0 && Y > 0)
+    {
+        Console.WriteLine("primeiro");
+    }
+    else if (X > 0 && Y < 0)
+    {
+        Console.WriteLine("quarto");
+    }
+    else if (X < 0 && Y < 0)
+    {
+        Console.WriteLine("terceiro");
+    }
+    else
+    {
+        Console.WriteLine("segundo");
+    }
+
+    vet = Console.ReadLine().Split(' ');
+    X = int.Parse(vet[0]);
+    Y = int.Parse(vet[1]);
+}
