97cb9f6987aa7916c2d4c80ac4d565fb06ec5ce7
Felipe Speranza <felipesperanza@hotmail.com>
2024-12-17 07:23:05 -0300

    Finalizando Módulo de estruturas repetitivas

---
+++ EstruturaRepetitiva/Exercicio3/Program.cs
@@ -0,0 +1,6 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i + " X " + N + " = " + i*N);
+}
---
+++ EstruturaRepetitiva/Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+
+int min, max, soma = 0;
+
+if (X < Y)
+{
+    min = X;
+    max = Y;
+}
+else
+{
+    min = Y;
+    max = X;
+}
+
+for (int i = min + 1; i < max; i++)
+{
+    if (i % 2 != 0)
+    {
+        soma += i;
+    }
+}
+
+Console.WriteLine(soma);
